0857e8392538abdaa2427c7080493a86cfb07e1e
Jaime Jose Cueva Sánchez <jcueva@jcueva-ibm.local>
2022-03-28 10:38:00 -0500

    Cambios

--- storage.tf
+++ storage.tf
@@ -1,9 +1,9 @@
 resource "ibm_is_volume" "vpc_cloudpak_volume" {
-  count = length(var.zones)
+  count = length(var.cloudpak_zones_vsi)
   name = "volumen-${var.project}-cp-${var.environment}-${format("%03s", count.index + 1)}"
   profile = "10iops-tier"
   capacity = var.cloudpak_storage_capacity
   resource_group = data.ibm_resource_group.resourceGroup.id
-  zone = var.zones[count.index]
+  zone = var.cloudpak_zones_vsi[count.index]
   tags = var.tags
 }
