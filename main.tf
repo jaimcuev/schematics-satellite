d49726d90e6f0b9ea0513c7af0f7a21b2dbeba89
Jaime Jose Cueva Sánchez <jcueva@jcueva-ibm.local>
2022-03-28 14:39:27 -0500

    Cambios

--- main.tf
+++ main.tf
@@ -78,3 +78,25 @@ resource "ibm_is_instance" "vpc_cloudpak_vsi" {
     delete = "15m"
   }
 }
+
+resource "ibm_is_instance" "vpc_odf_vsi" {
+  count = length(var.odf_zones_vsi)
+  name = "vm-${var.project}-odf-${var.environment}-${format("%03s", count.index + 1)}"
+  image = data.ibm_is_image.image_vm.id
+  profile = var.cloudpak_profile
+  resource_group = data.ibm_resource_group.resourceGroup.id
+  primary_network_interface {
+    subnet = ibm_is_subnet.vpc_subnet[index(ibm_is_subnet.vpc_subnet.*.zone, var.odf_zones_vsi[count.index])].id
+    allow_ip_spoofing = false
+  }
+  vpc = ibm_is_vpc.vpc_vm.id
+  zone = var.odf_zones_vsi[count.index]
+  volumes = [ibm_is_volume.vpc_odf_1_volume[count.index].id, ibm_is_volume.vpc_odf_2_volume[count.index].id]
+  keys = var.key
+  tags = var.tags
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
--- storage.tf
+++ storage.tf
@@ -7,3 +7,23 @@ resource "ibm_is_volume" "vpc_cloudpak_volume" {
   zone = var.cloudpak_zones_vsi[count.index]
   tags = var.tags
 }
+
+resource "ibm_is_volume" "vpc_odf_1_volume" {
+  count = length(var.odf_zones_vsi)
+  name = "volumen-${var.project}-odf-${var.environment}-${format("%03s", count.index + 1)}"
+  profile = "10iops-tier"
+  capacity = 100
+  resource_group = data.ibm_resource_group.resourceGroup.id
+  zone = var.odf_zones_vsi[count.index]
+  tags = var.tags
+}
+
+resource "ibm_is_volume" "vpc_odf_2_volume" {
+  count = length(var.odf_zones_vsi)
+  name = "volumen-${var.project}-odf-${var.environment}-${format("%03s", count.index + 1 + length(var.odf_zones_vsi))}"
+  profile = "10iops-tier"
+  capacity = 500
+  resource_group = data.ibm_resource_group.resourceGroup.id
+  zone = var.odf_zones_vsi[count.index]
+  tags = var.tags
+}
